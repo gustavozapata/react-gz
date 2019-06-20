fe524e4792fa013c95957c9646fc5e5cba50b937
Gustavo Zapata <tavordie@hotmail.com>
2019-06-20 21:54:01 +0100

    added color to Poll options + revote

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,23 +1,100 @@
 import React, { Component } from "react";
 
 class Poll extends Component {
-  state = {};
+  state = {
+    sel: false,
+    submited: false,
+    options: ["windows", "macos", "linux"]
+  };
+
+  optionSelect = e => {
+    this.setState({ sel: !this.state.sel });
+
+    //THIS MAY HELP TO SELECT THE BUTTON CLICKED
+    // this.setState({
+    //   tasks: [
+    //     ...this.state.tasks.map(task => {
+    //       if (task.id === id) {
+    //         task.task = e.target.value;
+    //       }
+    //       return task;
+    //     })
+    //   ]
+    // });
+    // END
+  };
+
+  activeStyle = () => {
+    return {
+      backgroundColor: this.state.sel ? "var(--orange)" : null
+    };
+  };
+
+  submitPoll = () => {
+    if (this.state.sel) {
+      this.setState({ submited: true });
+    }
+  };
+
+  resubmit = () => {
+    this.setState({ submited: false, sel: !this.state.sel });
+  };
+
   render() {
     return (
       <div className="poll">
         <h5>num of voters: 21,954</h5>
         <h1>POLL</h1>
-        <p>What operating system do you use for development purposes?</p>
-        <div style={{ margin: "10px auto", textAlign: "center" }}>
-          <span>
-            <button>Windows</button>
-            <button>macOS</button>
-          </span>
-          <span>
-            <button>Linux</button>
-          </span>
-        </div>
-        <button id="pollSubmit">Submit</button>
+        {this.state.submited ? (
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h4>Thanks for voting!</h4>
+            <h5
+              style={{
+                color: "white",
+                textDecoration: "underline",
+                marginTop: "40px",
+                cursor: "pointer"
+              }}
+              onClick={this.resubmit}
+            >
+              Vote again
+            </h5>
+          </div>
+        ) : (
+          <div>
+            <p>What operating system do you use for development purposes?</p>
+            <div style={{ margin: "10px auto", textAlign: "center" }}>
+              <span>
+                <button
+                  style={this.activeStyle()}
+                  id="windows"
+                  onClick={this.optionSelect}
+                >
+                  Windows
+                </button>
+                <button id="macos" onClick={this.optionSelect}>
+                  macOS
+                </button>
+              </span>
+              <span>
+                <button id="linux" onClick={this.optionSelect}>
+                  Linux
+                </button>
+              </span>
+            </div>
+            <button
+              onClick={this.submitPoll}
+              style={
+                this.state.sel
+                  ? { backgroundColor: "white", color: "black" }
+                  : null
+              }
+              id="pollSubmit"
+            >
+              Submit
+            </button>{" "}
+          </div>
+        )}
       </div>
     );
   }
