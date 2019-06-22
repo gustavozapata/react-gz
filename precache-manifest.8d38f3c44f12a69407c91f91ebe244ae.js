b4a0ae60b3551c140ba6ccc2e6eb7f378a8ee34a
Gustavo Zapata <tavordie@hotmail.com>
2019-06-22 18:04:13 +0100

    Updates

---
+++ precache-manifest.8d38f3c44f12a69407c91f91ebe244ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5659f67588b12dc51bf3fd580efed3",
+    "url": "/react-gz/index.html"
+  },
+  {
+    "revision": "b0257c0ebeb11d36cd64",
+    "url": "/react-gz/static/css/main.62d86b5e.chunk.css"
+  },
+  {
+    "revision": "d783692bcb22472dd49b",
+    "url": "/react-gz/static/js/2.30401af8.chunk.js"
+  },
+  {
+    "revision": "b0257c0ebeb11d36cd64",
+    "url": "/react-gz/static/js/main.e72b8467.chunk.js"
+  },
+  {
+    "revision": "3c67132dd790d5894cc7",
+    "url": "/react-gz/static/js/runtime~main.f3141b07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gz/precache-manifest.4e2016669ee3bffff6fdee8da546a186.js"
+  "/react-gz/precache-manifest.8d38f3c44f12a69407c91f91ebe244ae.js"
 );
 
 self.addEventListener('message', (event) => {
